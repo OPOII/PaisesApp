1f33f65d848f8f437e5914b9df040c77d361abc0
OPOII <41926039+OPOII@users.noreply.github.com>
2022-07-03 18:25:49 -0500

    move the logic of the sidebar to the component sidebar and fix the problems that should show up

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
-
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [
-    SidebarComponent
-  ],
-  imports: [
-    CommonModule
-  ],exports:[
-    SidebarComponent
-  ]
+  declarations: [SidebarComponent],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [SidebarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
