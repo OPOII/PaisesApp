1b03487f8edc5dde8cbb99417eeea5a7d18cc942
OPOII <41926039+OPOII@users.noreply.github.com>
2022-07-03 21:42:05 -0500

    Catching errors when the request got an error

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -9,13 +9,21 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent implements OnInit {
   //Termino es la variable que se le asigno al ngModel
   termino: string = '';
+  hayError: boolean = false;
   constructor(private paisService: PaisService) {}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
   buscar() {
-    this.paisService.buscarPais(this.termino).subscribe((respuesta) => {
-      console.log(respuesta);
-    });
+    this.hayError = false;
+    this.paisService.buscarPais(this.termino).subscribe(
+      (respuesta) => {
+        console.log(respuesta);
+      },
+      (err) => {
+        this.hayError = true;
+        console.log(err);
+      }
+    );
   }
 }
