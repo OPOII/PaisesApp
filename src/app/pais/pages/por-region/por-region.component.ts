24d4c73d510b19a6594be6bd1d7f1a3d8bb43dab
OPOII <41926039+OPOII@users.noreply.github.com>
2022-07-04 15:43:51 -0500

    Learning how to desestruct the css into methods in the component

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -12,11 +12,31 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class PorRegionComponent {
-  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regiones: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'EEU',
+    'AL',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC',
+  ];
   regionActiva: string = '';
   constructor() {}
 
   activarRegion(region: string) {
     this.regionActiva = region;
+  }
+
+  getClassCss(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
   }
 }
