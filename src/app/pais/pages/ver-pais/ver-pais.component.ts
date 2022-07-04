5c2eaf1eec09884c81aa2cc4673ca61a326e2453
OPOII <41926039+OPOII@users.noreply.github.com>
2022-07-04 01:41:13 -0500

    get the information of the country using the id of the country and searching in the API using two observables one inside the other

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private searchCountryService: PaisService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      this.searchCountryService.getCountryByCode(id).subscribe((pais) => {
+        console.log(pais);
+      });
+    });
   }
-
 }
