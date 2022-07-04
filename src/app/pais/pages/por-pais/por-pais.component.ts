05c14a4dc89e4923b8d6154884ba602ef6c58ec8
OPOII <41926039+OPOII@users.noreply.github.com>
2022-07-03 19:33:18 -0500

    Creating the by-country component, adding the variable to extract the info of the user input and the method that we are going to use to search in the api url

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PorPaisComponent implements OnInit {
-
-  constructor() { }
-
+  //Termino es la variable que se le asigno al ngModel
+  termino: string = '';
+  constructor() {}
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
-
+  buscar() {
+    console.log(this.termino);
+  }
 }
