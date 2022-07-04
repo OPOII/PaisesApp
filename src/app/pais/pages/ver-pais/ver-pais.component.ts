8602ce70d98606fc1ee6ef1700f29ec61d4ee9fd
OPOII <41926039+OPOII@users.noreply.github.com>
2022-07-04 01:50:58 -0500

    Learning about the switch map in the suscribes methods

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
@@ -14,10 +14,18 @@ export class VerPaisComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({ id }) => {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.searchCountryService.getCountryByCode(id))
+      )
+      .subscribe((resp) => {
+        console.log(resp);
+      });
+    /*this.activatedRoute.params.subscribe(({ id }) => {
       this.searchCountryService.getCountryByCode(id).subscribe((pais) => {
         console.log(pais);
       });
     });
+    */
   }
 }
