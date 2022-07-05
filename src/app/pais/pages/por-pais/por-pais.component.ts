70295675cab80486ede37020941e076a3ef1fc6f
OPOII <41926039+OPOII@users.noreply.github.com>
2022-07-04 19:56:25 -0500

    Showing how to create your own suggestions on angular

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,17 +5,26 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorPaisComponent implements OnInit {
   //Termino es la variable que se le asigno al ngModel
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
   byCountry = 'Buscar país...';
+  mostrarSugerencias: boolean = false;
   constructor(private paisService: PaisService) {}
   ngOnInit(): void {}
   buscar(termino: string) {
+    this.mostrarSugerencias = false;
     this.termino = termino;
     this.hayError = false;
     this.paisService.buscarPais(this.termino).subscribe(
@@ -33,5 +42,13 @@ export class PorPaisComponent implements OnInit {
   sugerencias(termino: string) {
     this.hayError = false;
     this.termino = termino;
+    this.mostrarSugerencias = true;
+    this.paisService.buscarPais(termino).subscribe(
+      (paises) => (this.paisesSugeridos = paises.splice(0, 5)),
+      (err) => (this.paisesSugeridos = [])
+    );
+  }
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
   }
 }
